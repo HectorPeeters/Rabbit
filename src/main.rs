6237fcab06115b94e47ecca84029febc8bbca663
Hector Peeters <hector.peeters@gmail.com>
2020-06-16 23:20:56 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+// extern crate markdown;
+
+// use markdown::{Block, ListItem, Span};
+use std::fs;
+
+mod markdown;
+use markdown::*;
+
+use katex;
+
+// fn convert_span_html(span: &Span) -> String {
+//     let mut result: String = String::default();
+
+//     match span {
+//         Span::Emphasis(children) => {
+//             result.push_str("<em>");
+//             for child in children {
+//                 result.push_str(&convert_span_html(child));
+//             }
+//             result.push_str("</em>");
+//         }
+//         Span::Strong(children) => {
+//             result.push_str("<strong>");
+//             for child in children {
+//                 result.push_str(&convert_span_html(child));
+//             }
+//             result.push_str("</strong>");
+//         }
+//         Span::Text(text) => {
+//             result.push_str(&text);
+//         }
+//         Span::Link(title, url, unknown) => {
+//             result.push_str(&format!("<a href=\"{}\">{}</a>", url, title));
+//         }
+//         Span::Image(title, url, unknown) => {
+//             result.push_str(&format!("<img src=\"{}\" title=\"{}\">", url, title));
+//         }
+//         _ => println!("Unhandleded span {:?}", span),
+//     }
+
+//     result
+// }
+
+// fn convert_block_html(block: &Block) -> String {
+//     let mut result: String = String::default();
+
+//     match block {
+//         Block::Header(children, size) => {
+//             result.push_str(&format!("<h{}>", size));
+//             for child in children {
+//                 result.push_str(&convert_span_html(child));
+//             }
+//             result.push_str(&format!("</h{}>", size));
+//         }
+//         Block::Paragraph(children) => {
+//             for child in children {
+//                 result.push_str(&convert_span_html(child));
+//             }
+//             result += "\n";
+//         }
+//         Block::UnorderedList(children) => {
+//             result.push_str("<ul>");
+//             for child in children {
+//                 result.push_str("<li>");
+//                 match child {
+//                     ListItem::Simple(span_children) => {
+//                         for s in span_children {
+//                             result.push_str(&convert_span_html(s));
+//                         }
+//                     }
+//                     ListItem::Paragraph(paragraph_children) => {
+//                         for p in paragraph_children {
+//                             result.push_str(&convert_block_html(p));
+//                         }
+//                     }
+//                 }
+//                 result.push_str("</li>");
+//             }
+//             result.push_str("</ul>");
+//         }
+//         _ => println!("Unhandleded block {:?}", block),
+//     }
+
+//     result
+// }
+
+fn to_html(node: MarkdownNode) -> String {
+    let mut result = String::default();
+
+    match node {
+        MarkdownNode::Header(text, level) => {
+            result.push_str(&format!("<h{}>{}</h{}>", level, text, level));
+        }
+        MarkdownNode::Paragraph(text) => {
+            result.push_str(&format!("<p>{}</p>", text));
+        }
+        MarkdownNode::List(items) => {
+            result.push_str("<ul>");
+            for text in items {
+                match text {
+                    MarkdownListItem::ListItem(text) => {
+                        result.push_str(&format!("<li>{}</li>", text));
+                    }
+                }
+            }
+            result.push_str("</ul>");
+        }
+        MarkdownNode::Math(math) => {
+            result.push_str("<center>");
+            result.push_str(katex::render(math).unwrap());
+            result.push_str("</center>");
+        }
+    }
+
+    result
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let header = include_str!("header.html");
+    let footer = include_str!("footer.html");
+
+    let markdown = fs::read_to_string("test_simple.md")?;
+
+    let parser: Parser = Parser::new(&markdown);
+
+    let mut result = String::from(header);
+
+    for node in parser {
+        let html = to_html(node);
+        // println!("{}", html);
+        result.push_str(&html);
+    }
+
+    result.push_str(footer);
+
+    fs::write("index.html", result)?;
+
+    Ok(())
+}
---
+++ src/markdown.rs
@@ -0,0 +1,140 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub enum MarkdownNode {
+    Header(String, usize),
+    Paragraph(String),
+    List(Vec<MarkdownListItem>),
+    Math(String),
+}
+
+pub enum MarkdownListItem {
+    ListItem(String),
+}
+
+pub struct Parser<'a> {
+    data: Vec<&'a str>,
+    index: usize,
+}
+
+fn is_whitespace(string: String) -> bool {
+    string == " " || string == "\t"
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Parser {
+            data: UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>(),
+            index: 0,
+        }
+    }
+
+    fn eof(&mut self) -> bool {
+        self.index >= self.data.len()
+    }
+
+    fn peek(&mut self, index: usize) -> String {
+        self.data[self.index + index].to_owned()
+    }
+
+    fn consume(&mut self) -> &str {
+        let result = self.data[self.index];
+        self.index += 1;
+        result
+    }
+
+    fn push_back(&mut self, count: usize) {
+        self.index -= count;
+    }
+
+    fn consume_char(&mut self, character: &str) -> String {
+        let mut result = String::default();
+
+        loop {
+            let c = self.peek(0);
+
+            if c != character {
+                break;
+            }
+
+            result.push_str(self.consume());
+        }
+
+        result
+    }
+
+    fn consume_until(&mut self, f: fn(String) -> bool) -> String {
+        let mut result = String::default();
+
+        loop {
+            if self.eof() {
+                break;
+            }
+
+            let c = self.peek(0);
+
+            if f(c) {
+                break;
+            }
+
+            result.push_str(self.consume());
+        }
+
+        result
+    }
+
+    fn skip_whitespace(&mut self) {
+        self.consume_until(|c| c != " " && c != "\t" && c != "\n" && c != "\r");
+    }
+
+    pub fn next_node(&mut self) -> Option<MarkdownNode> {
+        self.skip_whitespace();
+
+        if self.eof() {
+            return None;
+        }
+
+        let current_char = self.peek(0);
+        if current_char == "#" {
+            let hashtags = self.consume_char("#");
+
+            if is_whitespace(self.peek(0)) {
+                let header_name =
+                    String::from(self.consume_until(|c| c == "\n" || c == "\r").trim());
+                return Some(MarkdownNode::Header(header_name, hashtags.len()));
+            } else {
+                self.push_back(hashtags.len());
+            }
+        } else if current_char == "*" {
+            let mut nodes: Vec<MarkdownListItem> = Vec::new();
+
+            if is_whitespace(self.peek(1)) {
+                while !self.eof() && self.peek(0) == "*" {
+                    self.consume();
+
+                    let text = String::from(self.consume_until(|c| c == "\n" || c == "\r").trim());
+                    nodes.push(MarkdownListItem::ListItem(text));
+
+                    self.skip_whitespace();
+                }
+
+                return Some(MarkdownNode::List(nodes));
+            }
+        } else if current_char == "$" {
+            let math = self.consume_until(|c| c == "$");
+
+            return Some(MarkdownNode::Math(math));
+        }
+
+        let text = String::from(self.consume_until(|c| c == "\n").trim());
+
+        return Some(MarkdownNode::Paragraph(text));
+    }
+}
+
+impl Iterator for Parser<'_> {
+    type Item = MarkdownNode;
+
+    fn next(&mut self) -> Option<MarkdownNode> {
+        self.next_node()
+    }
+}
